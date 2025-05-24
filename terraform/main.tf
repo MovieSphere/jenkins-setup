370a7ee004d7f439aea2035a0f43c2abe06d65b4
Jeffry Anderson Romero Uriol <jromerou2@upao.edu.pe>
2025-05-23 21:57:03 -0500

    Add Terraform VPC setup for Test and update Jenkins configurations
    Introduce a Terraform configuration to create a VPC with public and private subnets in AWS for a Jenkins pipeline. Simplify Jenkins plugin versions and update `.gitignore` to include `.env` files.

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+# Ejemplo de Terraform de la implementación de un vpc en AWS, para un pipeline de jenkins
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "jenkins-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "jenkins-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    Name = "jenkins-private-subnet"
+  }
+}
